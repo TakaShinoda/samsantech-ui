e288a120683ac6e895c28975685bee8472cd75a4
Takahiro <tkhr_621@docomo.ne.jp>
2021-09-06 19:26:44 +0900

    input text sample

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <ButtonSample />
   <DialogSample />
+  <InputTextSample />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import ButtonSample from './components/buttons/ButtonSample.vue'
 import DialogSample from './components/dialogs/DialogSample.vue'
+import InputTextSample from './components/forms/InputTextSample.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     ButtonSample,
-    DialogSample
+    DialogSample,
+    InputTextSample
   },
 })
 </script>
 
 <style>
-/* #app { */
-  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; */
-/* } */
+  /* text-align: center;
+  color: #2c3e50; */
+  margin: 5px;
+}
 </style>
