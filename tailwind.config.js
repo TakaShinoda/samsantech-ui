7da0141935e7fca1138c990b18047ecbb961a9f7
Takahiro <tkhr_621@docomo.ne.jp>
2021-09-08 00:04:41 +0900

    install tailwindcss-pseudo-elements postcss-nested

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    // extend: {},
+    position: ['after'],
+    transform: ['after'],
+    translate: ['after'],
+    inset: ['after'],
+    backgroundImage: ['after'],
+    gradientColorStops: ['after'],
   },
-  plugins: [],
+  plugins: [require('tailwindcss-pseudo-elements')],
 }
