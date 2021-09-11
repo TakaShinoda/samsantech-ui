a7341500aa26f22a48ab973c7b767b9fc11c630d
Takahiro <tkhr_621@docomo.ne.jp>
2021-09-11 12:31:45 +0900

    msw

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './tailwind.css'
+import { worker } from './mock'
+
+// 開発環境でのみmswを動作させる
+if(process.env.NODE_ENV === 'development') {
+    worker.start()
+} 
 
 createApp(App).mount('#app')
