0837e11c5dfe249ca62c29ce81876a094a56b40e
Takahiro <tkhr_621@docomo.ne.jp>
2021-09-20 18:40:36 +0900

    公開準備

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build:
+    process.env.NODE_ENV === 'development'
+    ? {}
+    : {
+      lib: {
+        // ライブラリモードの時に読み込むエントリーポイントとライブラリ名を指定
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'samsantech-ui'
+      },
+      rollupOptions: {
+        // バンドル生成時に依存関係をそのままにしておきたいもの
+        external: ['vue'],
+
+        output: {
+          globals: {
+            vue: 'Vue'
+          }
+        }
+      }
+    }
 })
