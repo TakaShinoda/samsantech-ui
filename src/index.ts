68b098344e786bf46f66af5fd8f08ccc27a2dbe0
Takahiro <tkhr_621@docomo.ne.jp>
2021-09-20 18:18:38 +0900

    ライブラリモード向けのエントリーポイント

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './tailwind.css';
 export { default as PrimaryButton } from './components/buttons/PrimaryButton.vue'
 export { default as SecondaryButton } from './components/buttons/SecondaryButton.vue'
-export { default as Dialog } from './components/dialog/Dialog.vue'
+export { default as Dialog } from './components/dialogs/Dialog.vue'
 export { default as InputText } from './components/forms/InputText.vue'
 export { default as InputRadio } from './components/forms/InputRadio.vue'
 export { default as ContentsLoader } from './components/loader/ContentsLoader.vue'
